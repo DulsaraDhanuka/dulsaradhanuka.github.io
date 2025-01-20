325946c9d25e5db24c05acbc99066f672789d42a
Dulsara Dhanuka <dulsara.dhanuka@gmail.com>
2025-01-20 14:04:30 +0530

    Project restart cleanup

--- src/main.js
+++ src/main.js
@@ -1,116 +1,221 @@
 import './style.css'
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
-import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
-import gsap from "gsap";
 import { Pane } from "tweakpane";
 
-
-const pane = new Pane({ title: "Debug" });
-
-const loadingManager = new THREE.LoadingManager();
-const textureLoader = new THREE.TextureLoader(loadingManager);
-
-const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
-
-//const material = new THREE.MeshNormalMaterial();
-//material.wireframe = true;
-const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture});
-
-const fontLoader = new FontLoader();
-fontLoader.load(
-    "/fonts/helvetiker_regular.typeface.json",
-    (font) => {
-        console.log('font loaded');
-        const textGeometry = new TextGeometry("Hello Donuts", { 
-            font: font,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: 6,
-            bevelEnabled: true,
-            bevelThickness: 0.03,
-            bevelSize: 0.02,
-            bevelOffset: 0,
-            bevelSegments: 4
-        });
-        
-        textGeometry.center();
-
-        textGeometry.computeBoundingBox();
-        console.log(textGeometry.boundingBox);
-        const text = new THREE.Mesh(textGeometry, material);
-        scene.add(text);
-
-        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-
-        console.time("donuts");
-        for (let i = 0; i < 300; i++) {
-            const donut = new THREE.Mesh(
-                donutGeometry,
-                material
-            );
-            donut.position.x = (Math.random() - 0.5) * 10;
-            donut.position.y = (Math.random() - 0.5) * 10;
-            donut.position.z = (Math.random() - 0.5) * 10;
-
-            donut.rotation.x = Math.random() * Math.PI;
-            donut.rotation.z = Math.random() * Math.PI;
-
-            const scale = Math.random();
-            donut.scale.set(scale, scale, scale);;
-
-            scene.add(donut);
-        }
-        console.timeEnd("donuts");
-    }
-);
-
-const scene = new THREE.Scene();
-
-const cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial());
-
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-};
-
-window.addEventListener("resize", () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(sizes.width, sizes.height);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.z = 3;
-scene.add(camera);
-
 const canvas = document.getElementById("canvas");
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.render(scene, camera);
 
-const clock = new THREE.Clock();
-
-const tick = () => {    
-    const elapsedTime = clock.getElapsedTime();
-
-    controls.update();
-
-    renderer.render(scene, camera);
-
-    window.requestAnimationFrame(tick);
-}
-
-tick();
+//import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
+//import gsap from "gsap";
+//import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper";
+//import RAPIER from  "@dimforge/rapier3d";
+//import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+//import { DRACOLoader } from  "three/examples/jsm/loaders/DRACOLoader";
+//
+///**
+// * Loaders
+// */
+//const gltfLoader = new GLTFLoader()
+//const textureLoader = new THREE.TextureLoader()
+//const cubeTextureLoader = new THREE.CubeTextureLoader()
+//
+///**
+// * Base
+// */
+//// Debug
+////const gui = new GUI()
+//const panel = new Pane({title: "Debug"});
+//const debugObject = {}
+//
+//// Canvas
+//const canvas = document.getElementById("canvas");
+//
+//// Scene
+//const scene = new THREE.Scene()
+//
+///**
+// * Update all materials
+// */
+//const updateAllMaterials = () =>
+//{
+//    scene.traverse((child) =>
+//    {
+//        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+//        {
+//            // child.material.envMap = environmentMap
+//            child.material.envMapIntensity = debugObject.envMapIntensity
+//            child.material.needsUpdate = true
+//            child.castShadow = true
+//            child.receiveShadow = true
+//        }
+//    })
+//}
+//
+///**
+// * Environment map
+// */
+//const environmentMap = cubeTextureLoader.load([
+//    '/textures/environmentMap/px.jpg',
+//    '/textures/environmentMap/nx.jpg',
+//    '/textures/environmentMap/py.jpg',
+//    '/textures/environmentMap/ny.jpg',
+//    '/textures/environmentMap/pz.jpg',
+//    '/textures/environmentMap/nz.jpg'
+//])
+//
+//environmentMap.colorSpace = THREE.SRGBColorSpace
+//
+//// scene.background = environmentMap
+//scene.environment = environmentMap
+//
+//debugObject.envMapIntensity = 0.4
+////gui.add(debugObject, 'envMapIntensity').min(0).max(4).step(0.001).onChange(updateAllMaterials)
+//panel.addBinding(debugObject, "envMapIntensity", {min: 0, max: 4, step: 0.001}).on("change", updateAllMaterials);
+//
+///**
+// * Models
+// */
+//let foxMixer = null
+//
+//gltfLoader.load(
+//    '/models/Fox/glTF/Fox.gltf',
+//    (gltf) =>
+//    {
+//        // Model
+//        gltf.scene.scale.set(0.02, 0.02, 0.02)
+//        scene.add(gltf.scene)
+//
+//        // Animation
+//        foxMixer = new THREE.AnimationMixer(gltf.scene)
+//        const foxAction = foxMixer.clipAction(gltf.animations[0])
+//        foxAction.play()
+//
+//        // Update materials
+//        updateAllMaterials()
+//    }
+//)
+//
+///**
+// * Floor
+// */
+//const floorColorTexture = textureLoader.load('textures/dirt/color.jpg')
+//floorColorTexture.colorSpace = THREE.SRGBColorSpace
+//floorColorTexture.repeat.set(1.5, 1.5)
+//floorColorTexture.wrapS = THREE.RepeatWrapping
+//floorColorTexture.wrapT = THREE.RepeatWrapping
+//
+//const floorNormalTexture = textureLoader.load('textures/dirt/normal.jpg')
+//floorNormalTexture.repeat.set(1.5, 1.5)
+//floorNormalTexture.wrapS = THREE.RepeatWrapping
+//floorNormalTexture.wrapT = THREE.RepeatWrapping
+//
+//const floorGeometry = new THREE.CircleGeometry(5, 64)
+//const floorMaterial = new THREE.MeshStandardMaterial({
+//    map: floorColorTexture,
+//    normalMap: floorNormalTexture
+//})
+//const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+//floor.rotation.x = - Math.PI * 0.5
+//scene.add(floor)
+//
+///**
+// * Lights
+// */
+//const directionalLight = new THREE.DirectionalLight('#ffffff', 4)
+//directionalLight.castShadow = true
+//directionalLight.shadow.camera.far = 15
+//directionalLight.shadow.mapSize.set(1024, 1024)
+//directionalLight.shadow.normalBias = 0.05
+//directionalLight.position.set(3.5, 2, - 1.25)
+//scene.add(directionalLight)
+//
+////gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
+////gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001).name('lightX')
+////gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001).name('lightY')
+////gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001).name('lightZ')
+//panel.addBinding(directionalLight, "intensity", {min: 0, max: 10, step: 0.001, label: "Light intensity"});
+//panel.addBinding(directionalLight.position, "x", {min: -5, max: 5, step: 0.001, label: "Light X"});
+//panel.addBinding(directionalLight.position, "y", {min: -5, max: 5, step: 0.001, label: "Light Y"});
+//panel.addBinding(directionalLight.position, "z", {min: -5, max: 5, step: 0.001, label: "Light Z"});
+//
+///**
+// * Sizes
+// */
+//const sizes = {
+//    width: window.innerWidth,
+//    height: window.innerHeight
+//}
+//
+//window.addEventListener('resize', () =>
+//{
+//    // Update sizes
+//    sizes.width = window.innerWidth
+//    sizes.height = window.innerHeight
+//
+//    // Update camera
+//    camera.aspect = sizes.width / sizes.height
+//    camera.updateProjectionMatrix()
+//
+//    // Update renderer
+//    renderer.setSize(sizes.width, sizes.height)
+//    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+//})
+//
+///**
+// * Camera
+// */
+//// Base camera
+//const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+//camera.position.set(6, 4, 8)
+//scene.add(camera)
+//
+//// Controls
+//const controls = new OrbitControls(camera, canvas)
+//controls.enableDamping = true
+//
+///**
+// * Renderer
+// */
+//const renderer = new THREE.WebGLRenderer({
+//    canvas: canvas,
+//    antialias: true
+//})
+//renderer.toneMapping = THREE.CineonToneMapping
+//renderer.toneMappingExposure = 1.75
+//renderer.shadowMap.enabled = true
+//renderer.shadowMap.type = THREE.PCFSoftShadowMap
+//renderer.setClearColor('#211d20')
+//renderer.setSize(sizes.width, sizes.height)
+//renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+//
+///**
+// * Animate
+// */
+//const clock = new THREE.Clock()
+//let previousTime = 0
+//
+//const tick = () =>
+//{
+//    const elapsedTime = clock.getElapsedTime()
+//    const deltaTime = elapsedTime - previousTime
+//    previousTime = elapsedTime
+//
+//    // Update controls
+//    controls.update()
+//
+//    // Fox animation
+//    if(foxMixer)
+//    {
+//        foxMixer.update(deltaTime)
+//    }
+//
+//    // Render
+//    renderer.render(scene, camera)
+//
+//    // Call tick again on the next frame
+//    window.requestAnimationFrame(tick)
+//}
+//
+//tick()
+//
